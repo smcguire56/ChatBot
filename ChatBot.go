de7aedac62a753a544c5e8c0c00a4472f9d8e1f7
SEAN MC GUIRE - Student <G00330886@gmit.ie>
2017-10-31 15:04:02 +0000

    added basic chat inputs

--- ChatBot.go
+++ ChatBot.go
@@ -1,5 +1,18 @@
 package main
-import "fmt"
+import (
+    "fmt"
+	"net/http"
+)
+
+func printString(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+
 func main() {
     fmt.Println("hello world")
+
+
+    http.HandleFunc("/", printString)
+	http.ListenAndServe(":8080", nil)
 }
